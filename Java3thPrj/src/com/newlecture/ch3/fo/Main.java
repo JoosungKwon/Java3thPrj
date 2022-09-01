4bff2b554918efe66e5e09f03e117d01d7743ffb
ict04-13 <ict04-13@DESKTOP-C2ETNGA>
2022-09-01 20:41:43 +0900

    오목 게임 구현 1일차 (콘솔 ver)

---
+++ Java3thPrj/src/com/newlecture/ch3/fo/Main.java
@@ -0,0 +1,32 @@
+package com.newlecture.ch3.fo;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int size ;
+		Scanner scan = new Scanner(System.in);
+		
+		// 안전한 입력을 받아서 사이즈 넘겨 주기
+		// 잘못된 값 입력시 디폴트로 설정?
+		while(true) {
+			try{
+				System.out.print("오목판의 사이즈를 입력하세요(12 ~ 100): ");
+				size = Integer.parseInt(scan.nextLine());
+				if(size < 12  || 100 < size) {
+					System.out.print("입력 범위를 넘어갔습니다. 다시 입력해주세요");
+					continue;
+				}
+				break;
+			} catch (Exception e) {
+				System.out.println("오류입니다. 다시 입력해주세요");
+			}
+		}
+		// 앞단 화면 만들기
+
+		OmokApp omokGame = new OmokApp(size) ;
+		omokGame.game();
+
+	}
+
+}
--- Java3thPrj/src/com/newlecture/ch3/fo/OmokApp.java
+++ Java3thPrj/src/com/newlecture/ch3/fo/OmokApp.java
@@ -1,39 +1,172 @@
 package com.newlecture.ch3.fo;
 
-public class OmokApp {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+class OmokApp {
+	
+	private String[][] omokTable ;
+	
+	public OmokApp() {
+		this(12);
+	}
+	public OmokApp(int size) {
+		omokTable = new String[size][size];
+		setting();
+	}
+	
+	protected void game() {
+		int x,y ;
+		Scanner scan = new Scanner(System.in);
 		
-		for(int y=1; y<=12; y++) {
+		/// 게임 시작 화면 
+		System.out.print("┌────────────────────┐\n");
+		System.out.print("│     Game Start     │\n");
+		System.out.print("└────────────────────┘\n");
+		
+		//부가적인 화면 꾸미기 요소
+//		System.out.print("오목 게임이 시작됩니다.");	
+		
+		display() ;
+		
+		
+		
+		
+		// 게임이 진행되는 지점 
+		String[] color = {"●","○"}; // 첫번째가 흑, 두번째가 백?
+		int order = 0;
+		main : while(true) {
 			
-			// 모서리
-			if(y<= 1 )
-				System.out.printf("%c",'┌'); // 왼쪽 위 모서리
-			else if(y>= 12 )
-				System.out.printf("%c",'└');// 왼쪽 아래 모서리
-			else 
-				System.out.printf("%c",'├'); // 왼쪽 옆
+			while(true) {
+				try{
+					System.out.print("놓으실 위치를 입력하세요: ");	
+					String[] Line = scan.nextLine().split(" ");
+					if(Line[0].equals("finish")) { // 탈출 조건
+						break main;
+					}
+					y = Integer.parseInt(Line[0]);
+					x = Integer.parseInt(Line[1]);
+					break;
+				} catch (Exception e) {
+					System.out.println("오류입니다. 다시 입력해주세요");
+					continue;
+				}
+			}		
 			
-			for(int x=1; x<=10; x++) {
-				if(y<= 1 ) // 맨위 부분
-					System.out.printf("%c",'┬');
-				else if(y>= 12) // 맨 아래 부분
-					System.out.printf("%c",'┴');
-				else  // 가운데 칸
-					System.out.printf("%c",'┼');
-			}
+			if(!(select(y-1,x-1,color[(order++)%2]))) {
+				System.out.println("잘못된 위치에 입력하셨습니다. 다시 입력해주세요");
+				order--;
+				continue;
+			};
 			
-			if(y<= 1 )
-				System.out.printf("%c",'┐'); // 오른쪽 위 모서리
-			else if(y>= 12 )
-				System.out.printf("%c",'┘'); // 오른쪽 아래 모서리
-			else 
-				System.out.printf("%c",'┤'); // 오른쪽 옆
+//			
+//			if(whoIsWin(y,x)==5) {
+//				System.out.printf("%d 번 사용자가 승리하였습니다.! 축하드립니다.!%n",(order-1)%2);
+//			}
+			display() ;
 			
-			System.out.println();
+			
+			// 경기의 승패를 구하라
+			
+			
 		}
+
+	}
+	
+	
+	private void setting() {
+				int size = omokTable.length;
+				// 모서리 작업
+				omokTable[0][0] = "┌";
+				omokTable[size-1][0] = "└";
+				omokTable[0][size-1] = "┐";
+				omokTable[size-1][size-1] = "┘";
+				
+				// 양 옆
+				for(int i=0; i<size-2;i++) {
+					omokTable[0][i+1] = "┬";
+					omokTable[size-1][i+1] = "┴";
+					omokTable[i+1][0] = "├";
+					omokTable[i+1][size-1] = "┤";
+					
+				}
+				
+				// 몸통
+				for(int y =0; y<size-2; y++) {
+					for(int x = 0; x<size-2; x++) {
+						omokTable[y+1][x+1]= "┼" ;
+					}
+				}
 		
 	}
+	
+	// 바둑판을 출력해주는 함수
+	private void display() {
+		
+		for(int y=0; y<omokTable.length; y++) {
+			for(int x=0; x<omokTable[0].length; x++) {
+				System.out.printf("%s", omokTable[y][x]);
+			}
+			System.out.println();
+		}
+	}
+	
+	// 선택한 좌표에 돌을 놓는 함수
+	private boolean select(int y, int x, String color) {
+		int size = omokTable.length ;
+		
+		if( 0 > x || x >=size || 0 > y || y >=size) return false ; // 좌표 잘못 입력시
+		if(omokTable[y][x]=="○"|| omokTable[y][x]=="●") return false ; // 같은 자리에 중복해서 둘시에
+		
+		omokTable[y][x] = color ;
+		return true ;
+	}
 
+//	private int whoIsWin(int y, int x) {
+//		//현재 둔 돌과 연결된 돌들의 개수를 찾는 함수
+//		 String[][] temp = omokTable.clone();
+//		 String color = temp[y][x] ;
+//		 int cnt = 1;
+//		 Queue<int[]> queue = new LinkedList<>(); 
+//		 // 방향 8개를 다 확인하기
+//	
+//	
+//		 return 0;
 }
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		
+		
+		
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
